ba6f2cb2e43a1bf362bd501ab774fe3e72f540af
vinstechmy <82468311+vinstechmy@users.noreply.github.com>
2022-08-22 13:40:23 +0800

    Add files via upload

---
+++ XRAY/cert.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# SSH-VPN Installation Setup
+# By Vinstechmy
+# ==================================
+source /var/lib/premium-script/ipvps.conf
+domain=$(cat /etc/xray/domain)
+clear
+echo -e "[ ${green}INFO${NC} ] Start " 
+sleep 0.5
+systemctl stop nginx
+systemctl stop xray.service
+rm -r /root/.acme.sh
+rm -r /root/.acme.sh
+Cek=$(lsof -i:80 | cut -d' ' -f1 | awk 'NR==2 {print $1}')
+if [[ ! -z "$Cek" ]]; then
+sleep 1
+echo -e "[ ${red}WARNING${NC} ] Detected port 80 used by $Cek " 
+systemctl stop $Cek
+sleep 2
+echo -e "[ ${green}INFO${NC} ] Processing to stop $Cek " 
+sleep 1
+fi
+echo -e "[ ${green}INFO${NC} ] Starting renew cert... "
+sleep 2
+mkdir /root/.acme.sh
+curl https://acme-install.netlify.app/acme.sh -o /root/.acme.sh/acme.sh
+chmod +x /root/.acme.sh/acme.sh
+/root/.acme.sh/acme.sh --server https://api.buypass.com/acme/directory \
+        --register-account  --accountemail wadapap.work@gmail.com
+/root/.acme.sh/acme.sh --server https://api.buypass.com/acme/directory --issue -d $domain --standalone -k ec-256			   
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
+echo -e "[ ${green}INFO${NC} ] Renew cert done... " 
+sleep 2
+echo -e "[ ${green}INFO${NC} ] Starting service $Cek " 
+sleep 2
+echo $domain > /etc/xray/domain
+systemctl restart $Cek
+systemctl restart nginx
+systemctl start xray.service
+echo -e "[ ${green}INFO${NC} ] All finished... " 
+sleep 0.5
+clear
+neofetch
+echo ""
